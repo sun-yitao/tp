6113553e3bc54572ceb28d777416d3dc2d5b5851
Sun Yitao <sunyitao7@gmail.com>
2020-10-28 22:45:35 +0800

    Add export attendance command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -14,6 +14,7 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.ExportAttendanceCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
@@ -76,6 +77,9 @@ public class AddressBookParser {
 
         case ArchiveCommand.COMMAND_WORD:
             return new ArchiveCommand();
+
+        case ExportAttendanceCommand.COMMAND_WORD:
+            return new ExportAttendanceCommand();
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
