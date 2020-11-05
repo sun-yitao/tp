6d8ba2f0ee8ac769e13d9c09eb33c4c6ed5594f8
Sun Yitao <sunyitao7@gmail.com>
2020-11-05 16:37:22 +0800

    Improve add command error handling

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.parser;
 
-
+import static seedu.address.commons.core.Messages.MESSAGE_DUPLICATE_PREFIXES;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_PREAMBLE_NOT_EMPTY;
+import static seedu.address.commons.core.Messages.MESSAGE_PREFIX_MISSING;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MATRIC_NUMBER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
@@ -9,6 +11,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -45,8 +48,24 @@ public class AddCommandParser implements Parser<AddCommand> {
         boolean isPreambleEmpty = argMultimap.getPreamble().isEmpty();
         boolean areDuplicatePrefixesPresent = checkDuplicatePrefixes(argMultimap);
 
-        if (!areAllPrefixesPresent || !isPreambleEmpty || areDuplicatePrefixesPresent) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        if (!areAllPrefixesPresent) {
+            Optional<Prefix> missingPrefix = getMissingPrefix(argMultimap, PREFIX_NAME, PREFIX_PHONE,
+                    PREFIX_EMAIL, PREFIX_TELEGRAM, PREFIX_MATRIC_NUMBER);
+            if (missingPrefix.isPresent()) {
+                throw new ParseException(String.format(MESSAGE_PREFIX_MISSING, missingPrefix.get().toString(),
+                        AddCommand.MESSAGE_USAGE));
+            } else {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+            }
+        } else if (!isPreambleEmpty) {
+            throw new ParseException(MESSAGE_PREAMBLE_NOT_EMPTY);
+        } else if (areDuplicatePrefixesPresent) {
+            Optional<Prefix> duplicatePrefix = getDuplicatePrefix(argMultimap);
+            if (duplicatePrefix.isPresent()) {
+                throw new ParseException(String.format(MESSAGE_DUPLICATE_PREFIXES, duplicatePrefix.get()));
+            } else {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+            }
         }
 
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
@@ -73,13 +92,25 @@ public class AddCommandParser implements Parser<AddCommand> {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
+    private static Optional<Prefix> getMissingPrefix(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes)
+                .filter(prefix -> argumentMultimap.getValue(prefix).isEmpty())
+                .findFirst();
+    }
+
     /**
      * Returns true if there are duplicate prefixes(except for tag) in the given {@code ArgumentMultimap}.
      */
-    private static boolean checkDuplicatePrefixes(ArgumentMultimap argumentMultimaps) {
+    private static boolean checkDuplicatePrefixes(ArgumentMultimap argumentMultimap) {
         Stream<Prefix> prefixes = Stream.of(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_TELEGRAM,
                 PREFIX_MATRIC_NUMBER);
-        return prefixes.anyMatch(prefix -> argumentMultimaps.getAllValues(prefix).size() > 1);
+        return prefixes.anyMatch(prefix -> argumentMultimap.getAllValues(prefix).size() > 1);
+    }
+
+    private static Optional<Prefix> getDuplicatePrefix(ArgumentMultimap argumentMultimap) {
+        Stream<Prefix> prefixes = Stream.of(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_TELEGRAM,
+                PREFIX_MATRIC_NUMBER);
+        return prefixes.filter(prefix -> argumentMultimap.getAllValues(prefix).size() > 1).findFirst();
     }
 
 }
--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_PREAMBLE_NOT_EMPTY;
+import static seedu.address.commons.core.Messages.MESSAGE_PREFIX_MISSING;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_EMAIL_DESC;
@@ -26,6 +27,9 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalPersons.AMY;
@@ -78,23 +82,21 @@ public class AddCommandParserTest {
 
     @Test
     public void parse_compulsoryFieldMissing_failure() {
-        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
-
         // missing name prefix
         assertParseFailure(parser, VALID_NAME_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB,
-                expectedMessage);
+                String.format(MESSAGE_PREFIX_MISSING, PREFIX_NAME, AddCommand.MESSAGE_USAGE));
 
         // missing phone prefix
         assertParseFailure(parser, NAME_DESC_BOB + VALID_PHONE_BOB + EMAIL_DESC_BOB,
-                expectedMessage);
+                String.format(MESSAGE_PREFIX_MISSING, PREFIX_PHONE, AddCommand.MESSAGE_USAGE));
 
         // missing email prefix
         assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + VALID_EMAIL_BOB,
-                expectedMessage);
+                String.format(MESSAGE_PREFIX_MISSING, PREFIX_EMAIL, AddCommand.MESSAGE_USAGE));
 
         // all prefixes missing
         assertParseFailure(parser, VALID_NAME_BOB + VALID_PHONE_BOB + VALID_EMAIL_BOB,
-                expectedMessage);
+                String.format(MESSAGE_PREFIX_MISSING, PREFIX_NAME, AddCommand.MESSAGE_USAGE));
     }
 
     @Test
@@ -136,6 +138,6 @@ public class AddCommandParserTest {
         // non-empty preamble
         assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
                 + TELEGRAM_DESC_BOB + MATRIC_NUMBER_DESC_BOB + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+                MESSAGE_PREAMBLE_NOT_EMPTY);
     }
 }
