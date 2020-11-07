259e99c8ace9df88e1275895052f3e6febb9f74f
rishi <rishi5154@gmail.com>
2020-11-07 12:02:35 +0800

    Reorganise ModelStub into stubs package

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -22,7 +22,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.Tasker;
-import seedu.address.model.consultation.Consultation;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
