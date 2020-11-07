83ef0258d9014f0bf01496839a7fb61dd81f9de8
Kwan Noel <noelkwan1998@gmail.com>
2020-11-07 23:26:17 +0800

    Add tests for delete consult

---
+++ src/test/java/seedu/address/logic/commands/DeleteConsultCommandTest.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
+ * {@code DeleteCommand}.
+ */
+public class DeleteConsultCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredConsultList().size() + 1);
+        DeleteConsultCommand deleteConsultCommand = new DeleteConsultCommand(outOfBoundIndex);
+
+        assertCommandFailure(deleteConsultCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        DeleteConsultCommand deleteFirstCommand = new DeleteConsultCommand(INDEX_FIRST_PERSON);
+        DeleteConsultCommand deleteSecondCommand = new DeleteConsultCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
+
+        // same values -> returns true
+        DeleteConsultCommand deleteFirstCommandCopy = new DeleteConsultCommand(INDEX_FIRST_PERSON);
+        assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(deleteFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(deleteFirstCommand.equals(null));
+
+        // different consultation -> returns false
+        assertFalse(deleteFirstCommand.equals(deleteSecondCommand));
+    }
+}
