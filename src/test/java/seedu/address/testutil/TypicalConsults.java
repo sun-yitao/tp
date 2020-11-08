864f3767e36f84fa4886507ce6c0d6ebd0780386
Christopher Goh <chrisgzf@gmail.com>
2020-11-08 17:30:46 +0800

    Refactor TypicalConsults to be more reusable

--- src/test/java/seedu/address/testutil/TypicalConsults.java
+++ src/test/java/seedu/address/testutil/TypicalConsults.java
@@ -10,6 +10,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_TIME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TIME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TYPE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TYPE_BOB;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,24 +21,44 @@ import seedu.address.model.Tasker;
 import seedu.address.model.consultation.Consultation;
 
 
-
 /**
  * A utility class containing a list of {@code Consultation} objects to be used in tests.
  */
 public class TypicalConsults {
 
-    public static final Consultation ALICE_PERSONAL_CONSULT = new ConsultationBuilder().withName("Alice Pauline")
-            .withDay("10/10/2020").withTime("18:00").withLocation("Tembusu College").withType("personal").build();
-    public static final Consultation BENSON_GROUP_CONSULT = new ConsultationBuilder().withName("Benson Meier")
-            .withDay("12/10/2020").withTime("10:00").withLocation("Kent Ridge Drive").withType("group").build();
+    public static final String ALICE_DAY = "10/10/2020";
+    public static final String ALICE_TIME = "18:00";
+    public static final String ALICE_LOCATION = "Tembusu College";
+    public static final String ALICE_CONSULT_TYPE = "personal";
+
+    public static final String BENSON_DAY = "12/10/2020";
+    public static final String BENSON_TIME = "10:00";
+    public static final String BENSON_LOCATION = "Kent Ridge Drive";
+    public static final String BENSON_CONSULT_TYPE = "group";
+
+    public static final Consultation ALICE_PERSONAL_CONSULT = new ConsultationBuilder()
+        .withName(ALICE.getName().toString())
+        .withDay(ALICE_DAY)
+        .withTime(ALICE_TIME)
+        .withLocation(ALICE_LOCATION)
+        .withType(ALICE_CONSULT_TYPE)
+        .build();
+
+    public static final Consultation BENSON_GROUP_CONSULT = new ConsultationBuilder()
+        .withName(BENSON.getName().toString())
+        .withDay(BENSON_DAY)
+        .withTime(BENSON_TIME)
+        .withLocation(BENSON_LOCATION)
+        .withType(BENSON_CONSULT_TYPE)
+        .build();
 
     // Manually added - Consultation's details found in {@code CommandTestUtil}
     public static final Consultation PERSONAL_CONSULT = new ConsultationBuilder().withName(VALID_NAME_AMY)
-            .withDay(VALID_DAY_AMY).withTime(VALID_TIME_AMY).withLocation(VALID_LOCATION_AMY)
-            .withType(VALID_TYPE_AMY).build();
+        .withDay(VALID_DAY_AMY).withTime(VALID_TIME_AMY).withLocation(VALID_LOCATION_AMY)
+        .withType(VALID_TYPE_AMY).build();
     public static final Consultation GROUP_CONSULT = new ConsultationBuilder().withName(VALID_NAME_BOB)
-            .withDay(VALID_DAY_BOB).withTime(VALID_TIME_BOB).withLocation(VALID_LOCATION_BOB)
-            .withType(VALID_TYPE_BOB).build();
+        .withDay(VALID_DAY_BOB).withTime(VALID_TIME_BOB).withLocation(VALID_LOCATION_BOB)
+        .withType(VALID_TYPE_BOB).build();
 
     private TypicalConsults() {
     } // prevents instantiation
