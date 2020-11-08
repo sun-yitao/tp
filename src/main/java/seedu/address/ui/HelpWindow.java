d1be3bf1955c773fdbc4554ba62e8fa470c3c5bc
Kwan Noel <noelkwan1998@gmail.com>
2020-11-08 16:31:49 +0800

    Update user guide link

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -15,8 +15,8 @@ import seedu.address.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://github.com/"
-            + "AY2021S1-CS2103T-F11-1/tp/blob/master/docs/UserGuide.md";
+    public static final String USERGUIDE_URL = "https://ay2021s1-cs2103t-f11-1.github.io/"
+            + "tp/UserGuide.html#quick-start";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
