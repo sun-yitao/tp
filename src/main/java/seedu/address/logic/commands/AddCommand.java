2d6b6f8886644f269f49f9a68f497853a0bad04a
Kwan Noel <noelkwan1998@gmail.com>
2020-11-08 17:44:47 +0800

    Indicate telegram is compulsory

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -25,7 +25,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_MATRIC_NUMBER + "MATRIC_NUMBER "
-            + "[" + PREFIX_TELEGRAM + "TELEGRAM_HANDLE] "
+            + PREFIX_TELEGRAM + "TELEGRAM_HANDLE "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
