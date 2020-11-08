be6c80feb554a28cd2eeecf2cb0af93a1e59188c
Christopher Goh <chrisgzf@gmail.com>
2020-11-08 18:34:28 +0800

    Add Time tests

--- src/test/java/seedu/address/model/consultation/TimeTest.java
+++ src/test/java/seedu/address/model/consultation/TimeTest.java
@@ -1,12 +1,16 @@
 package seedu.address.model.consultation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class TimeTest {
+
+    private static final String VALID_TIME_STRING = "12:00";
 
     @Test
     public void constructor_null_throwsNullPointerException() {
@@ -32,8 +36,66 @@ public class TimeTest {
         assertFalse(Time.isValidTime("1270")); // incorrect input
 
         // valid time
-        assertTrue(Time.isValidTime("12:00"));
+        assertTrue(Time.isValidTime(VALID_TIME_STRING));
         assertTrue(Time.isValidTime("23:59"));
         assertTrue(Time.isValidTime("00:00"));
+    }
+
+
+    @Test
+    public void equals() {
+        // Equality to null
+        assertNotEquals(Time.fromTimeString(VALID_TIME_STRING), null);
+
+        // Equality to objects of other classes / parent classes
+        assertNotEquals(Time.fromTimeString(VALID_TIME_STRING), new Object());
+    }
+
+    @Test
+    public void testEquals_isReflexive() {
+        Time x = Time.fromTimeString(VALID_TIME_STRING);
+        assertEquals(x, x);
+        assertEquals(x.hashCode(), x.hashCode());
+    }
+
+    @Test
+    public void testEquals_isSymmetric() {
+        Time x = Time.fromTimeString(VALID_TIME_STRING);
+        Time y = Time.fromTimeString(VALID_TIME_STRING);
+        assertEquals(x, y);
+        assertEquals(y, x);
+        assertEquals(x.hashCode(), y.hashCode());
+    }
+
+    @Test
+    public void testEquals_isTransitive() {
+        Time x = Time.fromTimeString(VALID_TIME_STRING);
+        Time y = Time.fromTimeString(VALID_TIME_STRING);
+        Time z = Time.fromTimeString(VALID_TIME_STRING);
+        assertEquals(x, y);
+        assertEquals(y, z);
+        assertEquals(x, z);
+        assertEquals(x.hashCode(), z.hashCode());
+    }
+
+    @Test
+    public void testCompareTo() {
+        // earlier time
+        assertEquals(-1,
+            Time.fromTimeString("11:59").compareTo(
+                Time.fromTimeString("12:00")
+            ));
+
+        // later time
+        assertEquals(1,
+            Time.fromTimeString("12:01").compareTo(
+                Time.fromTimeString("12:00")
+            ));
+
+        // same time
+        assertEquals(0,
+            Time.fromTimeString("12:00").compareTo(
+                Time.fromTimeString("12:00")
+            ));
     }
 }
